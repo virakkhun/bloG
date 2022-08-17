084704686bf33504bd6ae3eed9065ab9d9937101
virakkhun <rakmanoban@gmail.com>
2022-08-17 16:44:37 +0700

    change: image in error.tsx

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Error: React.FC = () => {
-  document.title = "Page Not Found"
+  document.title = 'Page Not Found'
 
   return (
-    <div className='flex flex-col justify-center items-center h-screen bg-black text-white'>
-      <img src='/src/assets/images/404.svg' className='w-1/2 h-1/2' alt='page not found!'/>
-      <p className='mt-5'>It's seem pretty dark down here.</p>
-      <Link to='/' className='py-2 px-5 text-black bg-action rounded mt-5'>Go back</Link>
+    <div className="flex flex-col justify-center items-center h-screen bg-black text-white">
+      <img
+        src="/src/assets/images/404.png"
+        className="w-1/4"
+        alt="page not found!"
+      />
+      <p className="mt-5">It's seem pretty dark down here.</p>
+      <Link to="/" className="py-2 px-5 text-black bg-action rounded mt-5">
+        Go back
+      </Link>
     </div>
   )
 }
