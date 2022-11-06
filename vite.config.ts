96b5032b0f04aea2f272c33a3bafd9581e131b8f
virakkhun <rakmanoban@gmail.com>
2022-11-06 11:25:24 +0700

    chore: update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  const isDev = mode === 'development'
   return {
     plugins: [react()],
     logLevel: 'info',
@@ -12,11 +13,9 @@ export default defineConfig(({ mode }) => {
       host: true,
       proxy: {
         '/api': {
-          target:
-            mode === 'development'
-              ? process.env.VITE_BASE_URL
-              : 'https://api-blog.virak.me/v1',
-          changeOrigin: true,
+          target: process.env.VITE_BASE_URL,
+          changeOrigin: !isDev,
+          secure: !isDev,
           rewrite: (path) => path.replace(/^\/api/, '')
         }
       }
