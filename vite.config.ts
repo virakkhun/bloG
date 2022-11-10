d426e09a24df2f4ae03f1daf064e8d043c66a7c6
virakkhun <rakmanoban@gmail.com>
2022-11-10 14:01:18 +0700

    chore: start server using serve

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
-  const isDev = mode === 'development'
+export default defineConfig(() => {
   return {
     plugins: [react()],
     logLevel: 'info',
     server: {
       port: 8000,
-      host: true,
-      proxy: {
-        '/api': {
-          target: process.env.VITE_BASE_URL,
-          changeOrigin: !isDev,
-          secure: !isDev,
-          rewrite: (path) => path.replace(/^\/api/, '')
-        }
-      }
+      host: true
     }
   }
 })
