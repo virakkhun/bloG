96746fe93ac824f0152ae6c38ecbb2227fff34ee
virakkhun <rakmanoban@gmail.com>
2022-11-10 22:19:14 +0700

    chore: refactor ui

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,7 +9,6 @@ import PostComponent from '../../components/posts/PostComponet'
 import { fetchPosts } from '../../features/post/getPostSlice'
 
 const Post: React.FC = () => {
-  document.title = 'All Posts - bloG'
   const { post, isLoading } = useSelector((state: RootState) => state.post)
   const dispatch = useDispatch<AppDispatch>()
   const effect = useRef(true)
@@ -47,28 +46,34 @@ const Post: React.FC = () => {
   }
 
   return (
-    <div className="text-primary my-10 w-full">
-      <div className="w-full relative z-10">
-        {post.map((p) => (
-          <div key={p.id}>
-            <PostComponent
-              body={p.body}
-              id={p.id ? p.id : 0}
-              title={p.title}
-              slug={p.slug}
-              isShowCommentButton={true}
-              authorImage={p.authorImage}
-              userName={p.name}
-              userId={p.authorId}
-              image={p.images}
-            />
+    <>
+      <head>
+        <meta property="og:description" content="Welcome to Blog" />
+        <title>All Posts - bloG</title>
+      </head>
+      <div className="text-primary my-10 w-full">
+        <div className="w-full relative z-10">
+          {post.map((p) => (
+            <div key={p.id}>
+              <PostComponent
+                body={p.body}
+                id={p.id ? p.id : 0}
+                title={p.title}
+                slug={p.slug}
+                isShowCommentButton={true}
+                authorImage={p.authorImage}
+                userName={p.name}
+                userId={p.authorId}
+                image={p.images}
+              />
+            </div>
+          ))}
+          <div className="sticky bottom-0 w-full">
+            <PostButton />
           </div>
-        ))}
-        <div className="sticky bottom-0 w-full">
-          <PostButton />
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
