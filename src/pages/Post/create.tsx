890e0b79f7df4300b6cec3d4dbd008895fdd20e1
virakkhun <rakmanoban@gmail.com>
2022-11-11 14:04:04 +0700

    chore: add react-helmet for seo & add meta tags

--- src/pages/Post/create.tsx
+++ src/pages/Post/create.tsx
@@ -1,20 +1,20 @@
 import React, { useRef, useState } from 'react'
+import Loading from '../../components/assets/Loading'
+import Icons from '../../components/Icons/Icons'
 import { useDispatch } from 'react-redux'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { AppDispatch, RootState } from '../../app/store'
-import Loading from '../../components/assets/Loading'
-import Icons from '../../components/Icons/Icons'
 import { createPost } from '../../features/post/createPostSlice'
 import { userInfo } from '../../utils/storage/userInfo'
+import { Helmet } from 'react-helmet'
 
 const CreatePost: React.FC = () => {
-  document.title = 'Create Post - bloG'
   window.scrollTo({
     behavior: 'smooth',
     top: 0
   })
-  let navigate = useNavigate()
+  const navigate = useNavigate()
   const [title, setTitle] = useState<string>('')
   const [desc, setDesc] = useState<string>('')
   const [slug, setSlug] = useState<string>('')
@@ -56,93 +56,114 @@ const CreatePost: React.FC = () => {
   }
 
   return (
-    <div className="flex items-center w-full">
-      <div className="w-full">
-        <div className="mt-5 mb-5" onClick={() => navigate(-1)}>
-          <Icons name="chevron-left" style="h-5 w-5 cursor-pointer" />
-        </div>
-        <div className="w-16 h-16 rounded-full">
-          <img
-            src={
-              userInfo().authorImage
-                ? userInfo().authorImage
-                : 'https://upload.wikimedia.org/wikipedia/en/8/86/Avatar_Aang.png'
-            }
-            alt="profile"
-            className="w-16 h-16 rounded-full border border-gray-300"
-          />
-        </div>
-        <p className="mt-2">
-          {userInfo().name === null ? 'Avatar' : userInfo().name}
-          <span className="bg-black rounded-md text-white px-1 ml-2">
-            {userInfo().status ? 'active' : 'sleep'}
-          </span>
-        </p>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-5 mt-10">
-          <div>
-            <input
-              autoFocus={true}
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              type="text"
-              id="title"
-              placeholder="Title..."
-              className="w-full bg-transparent text-primary p-2 border-b border-b-gray-300 outline-none focus:outline-none"
-              autoComplete="none"
+    <>
+      <Helmet>
+        <title>Create Post | bloG</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="bloG is a mini social media, a legit platform for everyone"
+        />
+        <meta
+          name="keywords"
+          content="mini platform, cambodia, siem reap, khmer, blog, khmer blog, kon khmer, KSK, khmer sl khmer"
+        />
+        <meta
+          property="og:description"
+          content="bloG is a mini social media, which make your day happier."
+        />
+        <meta property="og:title" content="bloG" />
+        <meta property="og:type" content="web application" />
+        <meta property="og:url" content={window.location.hostname} />
+      </Helmet>
+      <div className="flex items-center w-full">
+        <div className="w-full">
+          <div className="mt-5 mb-5" onClick={() => navigate(-1)}>
+            <Icons name="chevron-left" style="h-5 w-5 cursor-pointer" />
+          </div>
+          <div className="w-16 h-16 rounded-full">
+            <img
+              src={
+                userInfo().authorImage
+                  ? userInfo().authorImage
+                  : 'https://upload.wikimedia.org/wikipedia/en/8/86/Avatar_Aang.png'
+              }
+              alt="profile"
+              className="w-16 h-16 rounded-full border border-gray-300"
             />
           </div>
-          <div>
-            <input
-              value={slug}
-              onChange={(e) => setSlug(e.target.value)}
-              type="text"
-              id="title"
-              placeholder="#Slug..."
-              className="w-full bg-transparent text-primary p-2 border-b border-b-gray-300 outline-none focus:outline-none"
-              autoComplete="none"
-            />
-          </div>
-          <div>
-            <textarea
-              rows={5}
-              value={desc}
-              onChange={(e) => setDesc(e.target.value)}
-              id="desc"
-              placeholder="Description"
-              className="w-full bg-transparent text-primary p-2 border-b border-b-gray-300 outline-none focus:outline-none"
-              autoComplete="none"
-            />
-          </div>
-          {image !== '' ? (
-            <img src={image} alt="preview image" />
-          ) : (
+          <p className="mt-2">
+            {userInfo().name === null ? 'Avatar' : userInfo().name}
+            <span className="bg-black rounded-md text-white px-1 ml-2">
+              {userInfo().status ? 'active' : 'sleep'}
+            </span>
+          </p>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-5 mt-10">
             <div>
-              <label
-                htmlFor="image"
-                className="flex justify-center items-center py-2 border border-dashed border-orange-400 rounded-md cursor-pointer"
-              >
-                <Icons name="image" style="h-5 w-5 fill-primary" />
-              </label>
               <input
-                type="file"
-                id="image"
-                className="hidden"
-                onChange={(e) => loadImage(e.target.files)}
+                autoFocus={true}
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                type="text"
+                id="title"
+                placeholder="Title..."
+                className="w-full bg-transparent text-primary p-2 border-b border-b-gray-300 outline-none focus:outline-none"
+                autoComplete="none"
               />
             </div>
-          )}
+            <div>
+              <input
+                value={slug}
+                onChange={(e) => setSlug(e.target.value)}
+                type="text"
+                id="title"
+                placeholder="#Slug..."
+                className="w-full bg-transparent text-primary p-2 border-b border-b-gray-300 outline-none focus:outline-none"
+                autoComplete="none"
+              />
+            </div>
+            <div>
+              <textarea
+                rows={5}
+                value={desc}
+                onChange={(e) => setDesc(e.target.value)}
+                id="desc"
+                placeholder="Description"
+                className="w-full bg-transparent text-primary p-2 border-b border-b-gray-300 outline-none focus:outline-none"
+                autoComplete="none"
+              />
+            </div>
+            {image !== '' ? (
+              <img src={image} alt="preview image" />
+            ) : (
+              <div>
+                <label
+                  htmlFor="image"
+                  className="flex justify-center items-center py-2 border border-dashed border-orange-400 rounded-md cursor-pointer"
+                >
+                  <Icons name="image" style="h-5 w-5 fill-primary" />
+                </label>
+                <input
+                  type="file"
+                  id="image"
+                  className="hidden"
+                  onChange={(e) => loadImage(e.target.files)}
+                />
+              </div>
+            )}
 
-          <div className="mt-5">
-            <button
-              type="submit"
-              className="rounded-md bg-action w-full py-1 hover:text-action hover:bg-secondary transition-all flex justify-center items-center"
-            >
-              {isLoading ? <Loading /> : 'Post'}
-            </button>
-          </div>
-        </form>
+            <div className="mt-5">
+              <button
+                type="submit"
+                className="rounded-md bg-action w-full py-1 hover:text-action hover:bg-secondary transition-all flex justify-center items-center"
+              >
+                {isLoading ? <Loading /> : 'Post'}
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- src/pages/Post/detail.tsx
+++ src/pages/Post/detail.tsx
@@ -10,6 +10,7 @@ import Loading from '../../components/assets/Loading'
 import Spinner from '../../components/assets/Spinner'
 import Comment from '../../components/comments/comment'
 import Icons from '../../components/Icons/Icons'
+import { Helmet } from 'react-helmet'
 
 const PostDetail: React.FC = () => {
   const navigate = useNavigate()
@@ -58,59 +59,80 @@ const PostDetail: React.FC = () => {
   }
 
   return (
-    <div className="w-full">
-      <div className="my-5" onClick={() => navigate(-1)}>
-        <Icons name="chevron-left" style="h-5 w-5 cursor-pointer" />
-      </div>
-      <div className="mt-5">
-        <PostComponent
-          body={detail.body}
-          id={detail.id}
-          slug={detail.slug}
-          title={detail.title}
-          userId={detail.authorId}
-          isShowCommentButton={false}
-          userName={author.name}
-          authorImage={author.authorImage}
-          image={detail.images}
+    <>
+      <Helmet>
+        <title>{`${detail.title} | bloG`}</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="bloG is a mini social media, a legit platform for everyone"
         />
-      </div>
-      <div className="mt-5 relative">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={comment}
-            onChange={(e) => setComment(e.target.value)}
-            className="text-primary border border-primary px-1 py-2 w-full outline-none"
-            placeholder="comment..."
+        <meta
+          name="keywords"
+          content="mini platform, cambodia, siem reap, khmer, blog, khmer blog, kon khmer, KSK, khmer sl khmer"
+        />
+        <meta
+          property="og:description"
+          content="bloG is a mini social media, which make your day happier."
+        />
+        <meta property="og:title" content="bloG" />
+        <meta property="og:type" content="web application" />
+        <meta property="og:url" content={window.location.hostname} />
+      </Helmet>
+      <div className="w-full">
+        <div className="my-5" onClick={() => navigate(-1)}>
+          <Icons name="chevron-left" style="h-5 w-5 cursor-pointer" />
+        </div>
+        <div className="mt-5">
+          <PostComponent
+            body={detail.body}
+            id={detail.id}
+            slug={detail.slug}
+            title={detail.title}
+            userId={detail.authorId}
+            isShowCommentButton={false}
+            userName={author.name}
+            authorImage={author.authorImage}
+            image={detail.images}
           />
-          {isPosting ? (
-            <div className="absolute top-3 right-2">
-              <Loading />
-            </div>
-          ) : (
-            <button type="submit">
-              <Icons
-                name="airline"
-                style={
-                  comment === ''
-                    ? 'h-5 w-5 cursor-not-allowed absolute top-3 right-2 opacity-50 rotate-90'
-                    : 'absolute top-3 right-2 hover:fill-action fill-primary h-5 w-5 rotate-90 cursor-pointer transition-all'
-                }
-              />
-            </button>
-          )}
-        </form>
-      </div>
+        </div>
+        <div className="mt-5 relative">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+              className="text-primary border border-primary px-1 py-2 w-full outline-none"
+              placeholder="comment..."
+            />
+            {isPosting ? (
+              <div className="absolute top-3 right-2">
+                <Loading />
+              </div>
+            ) : (
+              <button type="submit">
+                <Icons
+                  name="airline"
+                  style={
+                    comment === ''
+                      ? 'h-5 w-5 cursor-not-allowed absolute top-3 right-2 opacity-50 rotate-90'
+                      : 'absolute top-3 right-2 hover:fill-action fill-primary h-5 w-5 rotate-90 cursor-pointer transition-all'
+                  }
+                />
+              </button>
+            )}
+          </form>
+        </div>
 
-      <div>
-        {comments.length === 0 ? (
-          <p className="text-center mt-3">No Comments</p>
-        ) : (
-          comments?.map((c) => <Comment comment={c.comment} key={c.id} />)
-        )}
+        <div>
+          {comments.length === 0 ? (
+            <p className="text-center mt-3">No Comments</p>
+          ) : (
+            comments?.map((c) => <Comment comment={c.comment} key={c.id} />)
+          )}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
